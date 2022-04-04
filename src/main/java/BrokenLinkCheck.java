d1c232610a9787781727b65b20c0872dbe98e030
burakergoren <burak.ergoren@sahibinden.com>
2022-04-04 13:58:56 +0300

    refactor

--- src/main/java/BrokenLinkCheck.java
+++ src/main/java/BrokenLinkCheck.java
@@ -1,18 +1,18 @@
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
-import lombok.SneakyThrows;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class BrokenLinkCheck {
 
-  private static final String baseUrl = "https://www.sahibinden.com/";
-  private static final HashMap<String, Integer> brokenUrls = new HashMap<>();
+  private static final String baseUrl = "https://www.greenpeace.org/turkey/";
+  private static final HashMap<String, Integer> brokenLinks = new HashMap<>();
+  private static final HashMap<String, String> catchedLinks = new HashMap<>();
 
-  @SneakyThrows
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     Connection connection = Jsoup.connect(baseUrl);
     Document doc = connection.get();
     doc.select("a")
@@ -30,28 +30,29 @@ public class BrokenLinkCheck {
             URL url = new URL(link);
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestProperty("User-Agent", "curl/7.64.1");
-            http.setConnectTimeout(5000);
-            http.setReadTimeout(5000);
+            http.setConnectTimeout(15000);
+            http.setReadTimeout(15000);
             http.setRequestMethod("GET"); //this is important, some websites don't allow head request
             System.out.println("link is: " + link + " status code :" + http.getResponseCode());
 
             if (http.getResponseCode() != 200) {
-              brokenUrls.put(link, http.getResponseCode());
+              brokenLinks.put(link, http.getResponseCode());
             }
 
             http.disconnect();
           } catch (Exception e) {
-            e.printStackTrace();
+            catchedLinks.put(link, e.toString());
           }
         });
 
-    if (!brokenUrls.isEmpty()) {
+    if (!brokenLinks.isEmpty() || !catchedLinks.isEmpty()) {
       System.out.println("\nJob is done. Failed url lists are: \n");
-      brokenUrls.forEach((k, v) -> System.out.println("Broken link is : " + k + ", status code = " + v));
+      brokenLinks.forEach((k, v) -> System.out.println("Broken link is : " + k + ", status code : " + v));
+      catchedLinks.forEach((k, v) -> System.out.println("Failed link is : " + k + ", error message : " + v));
     }
     else
     {
-      System.out.println("\n Job is done. There is no broken links");
+      System.out.println("\nJob is done. There is no broken links");
     }
   }
 }
